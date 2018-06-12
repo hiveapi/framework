bb0c9dcf36adf0e55de8929ffc22184806cdf9fe
Johannes Schobel <johannes.schobel@googlemail.com>
2018-06-12 08:31:51 +0200

    removing old tests and bootstrapping EMPTY tests for specific containers! Tests will be added in future :D

---
+++ app/Containers/Settings/Tests/Tests/_support/UnitTester.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Containers\Settings\Tests\Tests;
+
+use App\Containers\Settings\Tests\Tests\_generated\UnitTesterActions as ContainerUnitTesterActions;
+use App\Ship\Parents\Tests\Tester\Tester;
+use App\Ship\Parents\Tests\Traits\UnitTesterActions as ShipUnitTesterActions;
+
+class UnitTester extends Tester
+{
+    use ContainerUnitTesterActions;
+    use ShipUnitTesterActions;
+
+   /**
+    * Define custom methods for this tester here
+    */
+}
